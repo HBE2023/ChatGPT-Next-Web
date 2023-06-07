e7c91e2098dfbdd0a61e9f190afd91b2f9fe20c3
闫凯ol <18369679827@163.com>
2023-06-07 11:40:19 +0800

    second commit

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,7 +119,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT HB</div>
+        <div className={styles["sidebar-title"]}>ChatGPT HBT</div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own AI assistant.
         </div>
